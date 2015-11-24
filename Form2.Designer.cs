01a1de9ce179ce3cf165c4ba82cf851598e8c206
Dylan Chen <hjchen2005@gmail.com>
2015-11-23 21:41:59 -0500

    Create Form2.Designer.cs

---
+++ Form2.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PongGame
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playground = new System.Windows.Forms.Panel();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.racket = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.playground.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).BeginInit();
+            this.SuspendLayout();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form2_KeyDown);
+            // 
+            // playground
+            // 
+            this.playground.Controls.Add(this.ball);
+            this.playground.Controls.Add(this.racket);
+            this.playground.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playground.Location = new System.Drawing.Point(0, 0);
+            this.playground.Name = "playground";
+            this.playground.Size = new System.Drawing.Size(767, 528);
+            this.playground.TabIndex = 0;
+            
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Tomato;
+            this.ball.Location = new System.Drawing.Point(503, 104);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // racket
+            // 
+            this.racket.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.racket.Location = new System.Drawing.Point(228, 479);
+            this.racket.Name = "racket";
+            this.racket.Size = new System.Drawing.Size(200, 20);
+            this.racket.TabIndex = 0;
+            this.racket.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(767, 528);
+            this.Controls.Add(this.playground);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.playground.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.racket)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel playground;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox racket;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
